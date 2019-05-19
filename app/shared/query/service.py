9b687687f257fe52bca4592aac2487c446a9c652
Alan Pryor <APryor@ATD-US.com>
2019-05-19 11:52:41 -0400

    Add shared folder

---
+++ app/shared/query/service.py
@@ -0,0 +1,6 @@
+class QueryService:
+    '''An example of a service that is shared'''
+
+    @staticmethod
+    def execute(query):
+        return 'Success'
---
+++ app/shared/query/service_test.py
@@ -0,0 +1,7 @@
+from .service import QueryService
+
+
+def test_execute():
+    result = QueryService.execute('a complicated query')
+
+    assert result == 'Success'
